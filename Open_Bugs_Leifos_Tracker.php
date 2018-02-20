804605c752555055fa65976ccd6d18ffd7c7ab8e
Thomas Famula <famula@leifos.de>
2018-02-20 09:34:01 +0100

    Fixed issues #1 and #2

--- Open_Bugs_Leifos_Tracker.php
+++ Open_Bugs_Leifos_Tracker.php
@@ -2,7 +2,7 @@
 
 include("./leifos_mantis_config.php");
 
-function collectBugs() {
+function collectBugs($servername, $username, $password, $dbname, $url) {
 
     // Create and check connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -10,13 +10,18 @@ function collectBugs() {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Select all active users
-    $sql_users = "SELECT * FROM mantis_user_table WHERE enabled='1'";
+    $all_bugs = array();
+
+    // Select all active users except admin and users without email address
+    $sql_users = "SELECT * FROM mantis_user_table WHERE enabled='1' AND email <> '' AND email <> 'root@localhost'";
     $result_users = mysqli_query($conn, $sql_users);
 
     if (mysqli_num_rows($result_users) > 0) {
         while ($row_users = mysqli_fetch_assoc($result_users)) {
-            $message = "Hello " . $row_users["realname"] . ", \nYou have open bugs: \n\n";
+            $user_bug = array(
+                "email" => $row_users["email"],
+                "message" => "Hello " . $row_users["realname"] . ", \nYou have open bugs: \n\n"
+            );
 
             // Select bugs with status not equal to 'resolved' or 'closed' for current user
             $sql_users_bugs = "SELECT p.name AS projectname, b.summary AS bugname, b.id AS bugid, b.project_id, p.id 
@@ -28,35 +33,48 @@ function collectBugs() {
             $bug_number = 1;
             if (mysqli_num_rows($result_users_bugs) > 0) {
                 while ($row_users_bug = mysqli_fetch_assoc($result_users_bugs)) {
-                    $message = $message . $bug_number . ". " . $row_users_bug["projectname"] . ": " . $row_users_bug["bugname"] .
-                                " --> http://localhost/mantisbt-2.10.0/view.php?id=" . $row_users_bug["bugid"] . "\n";
+                    $message = $bug_number . ". " . $row_users_bug["projectname"] . ": " . $row_users_bug["bugname"] .
+                        " --> " . $url . $row_users_bug["bugid"] . "\n";
+                    $user_bug["message"] = $user_bug["message"] . $message;
                     $bug_number++;
                 }
+                array_push($all_bugs, $user_bug);
 
-                try {
-
-                    $to = $row_users["email"];
-                    $subject = "[LeifosBugTracker] Your current open bugs";
-                    $from = "From: Leifos <noreply@leifos.com>";
-
-                    mail($to, $subject, $message, $from);
-                    echo "Email sent to " . $row_users ["realname"] . "<br>";
-
-                }catch(Exception $e){
-                    echo 'Message could not be sent. Mailer Error: ', $e->ErrorInfo;
-                }
             } else {
-                echo "No bugs found for " . $row_users["realname"] . "<br>";
+                echo "No bugs found for " . $row_users["realname"] . "\n";
             }
-
         }
+
     } else {
-        echo "No users found <br>";
+        echo "No users found \n";
     }
 
     mysqli_close($conn);
+
+    //print_r($all_bugs);
+    sendEmail($all_bugs);
 }
 
-collectBugs();
+function sendEmail($all_bugs) {
+
+    try {
+
+        $subject = "[LeifosBugTracker] Your current open bugs";
+        $from = "From: Leifos <noreply@leifos.com>";
+
+        foreach ($all_bugs as $user_bug) {
+            $to = $user_bug["email"];
+            $message = $user_bug["message"];
+            mail($to, $subject, $message, $from);
+            echo "Email sent to " . $user_bug["email"] . "\n";
+        }
+        
+    }catch(Exception $e){
+        echo 'Message could not be sent. Mailer Error: ', $e->ErrorInfo;
+    }
+
+}
+
+collectBugs($servername, $username, $password, $dbname, $url);
 
 ?>
--- leifos_mantis_config.example.php
+++ leifos_mantis_config.example.php
@@ -1,8 +1,12 @@
 <?php
 
+// Database connection
 $servername = "your_servername";
 $username = "your_db_username";
 $password = "your_db_password";
 $dbname = "your_db_name";
+
+// For Mantis Bug URL
+$url = "https://yourMantisURL.com/view.php?id="
 
 ?>
